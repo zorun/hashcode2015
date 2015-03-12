c965300e71165b125d27f5534b69c9517092be72
Baptiste Jonglez <baptiste.jonglez@ens-lyon.fr>
2015-03-12 21:30:55 +0100

    Fix Python code (infinite loop, and silly bug)

--- python/dc.py
+++ python/dc.py
@@ -21,10 +21,10 @@ class Server(object):
         if None in [self.pool, self.row, self.slot]:
             return 'x'
         else:
-            return '{} {} {}'.format(self.row, self.slot, self.pool)
+            return '{} {} {}'.format(self.row, self.slot, self.pool.id)
 
     def __str__(self):
-        return 'Server({}, {}, {})'.format(self.size, self.cpu, self.pool)
+        return 'Server({}, {}, {})'.format(self.size, self.cpu, self.pool.id)
 
 
 class Row(object):
@@ -77,14 +77,14 @@ class Pool(object):
     
     def __init__(self, id):
         self.id = id
-        self.servers = list()
+        self.servers = set()
         self.cached_score = 0
         # For score caching
         self.dirty = False
 
     def add_server(self, serv):
         #print("Add server to pool {}".format(self.id))
-        self.servers.append(serv)
+        self.servers.add(serv)
         serv.pool = self
         self.dirty = True
 
@@ -108,7 +108,9 @@ class Pool(object):
         # CPU capacity per row
         cpu_row = defaultdict(int)
         for s in self.servers:
-            cpu_row[s.pool] += s.cpu
+            cpu_row[s.row] += s.cpu
+        #print(cpu_row)
+        #print("Score for pool {} is {} with {} servers".format(self.id, cpu - max(cpu_row.values()), len(self.servers)))
         return cpu - max(cpu_row.values())
 
 
@@ -152,28 +154,29 @@ class DC(object):
             pool.del_server(server)
             return None
         else:
-            print("Keeping optimal place {}".format(optimal_place))
+            #print("Keeping optimal place {0[0].id}, {0[1]}".format(optimal_place))
             optimal_place[0].add_server(server, optimal_place[1])
             return optimal_place
 
     def solve(self):
-        while True:
+        success = True
+        while success:
             worst_pool = min(self.pools, key=lambda p: p.score)
-            print("Working on pool {}, score {}".format(worst_pool.id, worst_pool.score))
+            #print("Working on pool {}, score {}".format(worst_pool.id, worst_pool.score))
             available_servers = [s for s in self.servers if s.pool == None]
             if available_servers == []:
                 return
             # Best server first
             for server in sorted(available_servers, key=lambda s: s.cpu / s.size, reverse=True):
+                #print("Trying server {}".format(server))
                 res = self.place_server(server, worst_pool)
                 if res != None: # Success
+                    success = True
+                    #print("Success!")
                     break
-            print(self.score())
-#        for (i, s) in enumerate(self.servers):
-#            self.rows[i % self.nb_rows].add_server(s, i)
-#            self.pools[i % self.nb_pools].add_server(s)
-#            print(self.pools[i % self.nb_pools].score)
-            #self.output()
+            else:
+                success = False
+            #print(self.score(), [p.score for p in self.pools])
 
     def display(self):
         for (i, r) in enumerate(self.rows):
