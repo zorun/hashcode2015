27e85f8350c384a0122237e4504adbf9d0b66c0a
Gabriele Romaniello <gabriele.romaniello@imag.fr>
2015-03-12 21:15:16 +0100

    basic pool

---
+++ java_hashcode/src/java_hashcode/pool.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_hashcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author gabriele
+ */
+public class pool {
+    
+
+    public pool(){
+	}
+        
+  public int add(Server s){           
+      
+      servers.add(s);
+            return 1;
+    }
+    
+        
+          
+               List<Server> servers = new ArrayList<Server>();
+}
