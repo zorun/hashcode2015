8acf0981c7b9fc98a0c30bdeb9936de22684f608
JUC Iacob <iacob.juc@gmail.com>
2015-03-28 15:37:24 +0100

    output 22000 point

--- loon/loon.py
+++ loon/loon.py
@@ -7,22 +7,44 @@ import sys
 
 if __name__ == '__main__':
     
-    k = range(0, 8, 1);
-    ##print(*i)
-    print(*([1]*53), end=' ')
-    print(*([0]*0))
+    print(*([1]*23), end=' ')
+    print(*([0]*30))
 
-    print(*([1]*48), end=' ')
-    print(*([0]*5))
+    print(*([0]*53))
+
+    print(*([0]*53))
+
+    print(*([0]*53))
+
+    print(*([1]*32), end=' ')
+    print(*([0]*21))
+
+    print(*([0]*53))
+
+    print(*([0]*53))
+
+    print(*([0]*53))
 
     print(*([1]*40), end=' ')
     print(*([0]*13))
+
+
+    print(*([0]*53))
+
+    print(*([0]*53))
+
+    print(*([0]*53))
     
-    print(*([1]*32), end=' ')
-    print(*([0]*21))
+    print(*([1]*48), end=' ')
+    print(*([0]*5))
+
+    print(*([1]*53))
+
+    
+
     
     print(*([1]*23), end=' ')
     print(*([0]*30))
-    for i in range(0, 395):
+    for i in range(0, 385):
         print(*([0]*53))
     
