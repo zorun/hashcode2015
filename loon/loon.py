0a3a25b740383e228844b89ef9da083b95cafc3e
JUC Iacob <iacob.juc@gmail.com>
2015-03-28 11:46:40 +0100

    dummy solution: all baloons in the air max altitude

---
+++ loon/loon.py
@@ -0,0 +1,15 @@
+## this is an empty python source file
+## if you are reading this: this file was submitted to accompagny our
+## hand written solution;
+
+import sys
+
+
+if __name__ == '__main__':
+    k = range(0, 8, 1);
+    ##print(*i)
+    for j in k:
+        print(*([1]*53))
+    for i in range(0, 392):
+        print(*([0]*53))
+    
