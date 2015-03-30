662b4447295ed2a23094938d74d84d648a37e631
Baptiste Jonglez <baptiste--git@jonglez.org>
2015-03-30 11:08:45 +0200

    Change graph: add weight to identify dummy edges, and a self-loop at the source

--- loon/graph.py
+++ loon/graph.py
@@ -13,22 +13,28 @@ Node = namedtuple("Node", ("row", "col", "alt", "input"))
 class LoonGraph(object):
     """Graph structure: each position (row, col, alt) is modelled as two nodes
     (True → input, False → output)
-    Input node: (row, col, alt, True)
-    Output node: (row, col, alt, False)
-    Sink: None
+    - Input node: (row, col, alt, True)
+    - Output node: (row, col, alt, False)
+    - Sink: None
+
+    Edges have a "weight" attribute: 1 means that an action is generated
+    when a balloon takes this edge (either going up, down, or staying at
+    the same altitude).  A weight of 0 means that this edge can be taken
+    freely, it does not correspond to any movement in reality.
     """
 
     def __init__(self, loon):
-        self.source = Node(row=loon.start_row, col=loon.start_col, alt=0, input=False)
+        self.source = Node(row=loon.start_row, col=loon.start_col, alt=0, input=True)
         self.sink = None
+        self.loon = loon
         if os.path.exists(GRAPH):
+            # Load graph from file if it is already computed
             self.g = nx.read_gpickle(GRAPH)
         else:
-            # Load graph from file if it is already computed
-            self.build_graph(loon)
+            self.build_graph()
             nx.write_gpickle(self.g, GRAPH)
 
-    def build_graph(self, loon):
+    def build_graph(self):
         g = nx.DiGraph()
         # Source
         g.add_node(self.source)
@@ -36,47 +42,48 @@ class LoonGraph(object):
         # Sink
         g.add_node(self.sink)
         g.node[self.sink]["nb_targets"] = 0
-        for alt in range(1, loon.altitudes + 1):
-            for row in range(loon.nb_rows):
-                for col in range(loon.nb_cols):
+        for alt in range(1, self.loon.altitudes + 1):
+            for row in range(self.loon.nb_rows):
+                for col in range(self.loon.nb_cols):
                     n_in = Node(row, col, alt, True)
                     n_out = Node(row, col, alt, False)
                     g.add_node(n_in)
                     g.add_node(n_out)
-                    g.add_edge(n_in, n_out)
+                    g.add_edge(n_in, n_out, weight=0)
                     # Link to other altitudes
                     if alt > 1:
-                        g.add_edge(n_in, Node(row, col, alt - 1, False))
-                    if alt < loon.altitudes:
-                        g.add_edge(n_in, Node(row, col, alt + 1, False))
+                        g.add_edge(n_in, Node(row, col, alt - 1, False), weight=1)
+                    if alt < self.loon.altitudes:
+                        g.add_edge(n_in, Node(row, col, alt + 1, False), weight=1)
                     # Wind
-                    vec = loon.winds[alt][row][col]
-                    if row + vec.drow >= loon.nb_rows or row + vec.drow < 0:
+                    vec = self.loon.winds[alt][row][col]
+                    if row + vec.drow >= self.loon.nb_rows or row + vec.drow < 0:
                         # Ballon is lost
-                        g.add_edge(n_out, self.sink)
+                        g.add_edge(n_out, self.sink, weight=1)
                     else:
-                        g.add_edge(n_out, Node(row + vec.drow, (col + vec.dcol) % loon.nb_cols, alt, True))
+                        g.add_edge(n_out, Node(row + vec.drow, (col + vec.dcol) % self.loon.nb_cols, alt, True), weight=1)
                     # Covered targets
                     here = utils.Point(row=row, col=col)
-                    targets = [t for t in loon.targets if loon.is_in_range(here, t)]
+                    targets = [t for t in self.loon.targets if self.loon.is_in_range(here, t)]
                     #print("{} targets at position ({}, {}, {})".format(len(targets), alt, row, col))
                     g.node[n_in]["nb_targets"] = g.node[n_out]["nb_targets"] = len(targets)
         # Connect source to graph
-        g.add_edge(self.source, Node(row=self.source.row, col=self.source.col, alt=1, input=True))
+        g.add_edge(self.source, Node(row=self.source.row, col=self.source.col, alt=1, input=True), weight=1)
+        # Self-edge on the source, to model the fact that we can wait before launching a balloon
+        g.add_edge(self.source, self.source, weight=1)
         self.g = g
 
     def path_to_movements(self, path):
         for (node1, node2) in zip(path, path[1:]):
-            #print(node1, node2)
-            if node1 == None or node2 == None:
+            if node1 == self.sink or node2 == self.sink:
                 yield 0
+            elif self.g[node1][node2][weight] == 0:
+                continue
             elif node2.alt > node1.alt:
                 yield 1
             elif node2.alt < node1.alt:
                 yield -1
-            elif node1.input == True: #skip the transition from input.True to input.False
-                continue
-            elif node2.alt == node1.alt:
+            else:
                 yield 0
 
     def test(self, node):
